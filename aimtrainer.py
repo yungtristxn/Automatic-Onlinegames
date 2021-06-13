2289f589273bb40bbf9ea5a6f415ebe2e411f8e8
yungtristxn <oetris@gmail.com>
2021-06-13 23:28:46 +0200

    Update aimtrainer.py
    made script a little bit faster

--- aimtrainer.py
+++ aimtrainer.py
@@ -1,21 +1,21 @@
 from pyautogui import locateCenterOnScreen
-import win32api
-import win32con
+from win32api import SetCursorPos,mouse_event
+from win32con import MOUSEEVENTF_LEFTDOWN,MOUSEEVENTF_LEFTUP
 import keyboard
 import os 
 
 
 def click(x, y):
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    SetCursorPos((x, y))
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0)
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0)
 
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
     target = fr'{dir_path}\Targets\Target.png'
     while True:
         try:    #region=(552, 302, 800, 470) for aimtrainer.io in fullscreen [1920x1080]  /// size of target is 5
-            x = tuple(locateCenterOnScreen(target,region=(552, 302, 800, 470), grayscale=True,  confidence=0.8))
+            x = tuple(locateCenterOnScreen(target,region=(552, 302, 800, 470),grayscale = True,  confidence=0.8))
             click(x[0],x[1])
         except TypeError:
             pass
